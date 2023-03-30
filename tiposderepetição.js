d8b68a8bf8797968b97871fee511af08d2fee052
Allan <allangabrieldev@gmail.com>
2023-03-30 17:15:51 -0300

    Estruturas de repetição Js

---
+++ tiposderepetição.js
@@ -0,0 +1,36 @@
+// sem uso de repetição
+
+ 
+console.log ('oi')
+console.log ('oi')
+console.log ('oi')
+console.log ('oi')
+
+
+// repetição tendo o teste lógico no início
+
+
+var c = 1
+while (c <= 4) {
+    console.log ('oi')
+    c++ // isso é igual a variavel c + 1
+} 
+
+
+// repetição com o teste lógico no final
+
+
+var c = 1
+do {
+    console.log('oi')
+    c++
+} while (c <= 4)
+
+
+// repetição com variável de controle
+
+for (var c = 1; c <= 4; c++){
+    console.log('oi')
+}
+
+//TODOS OS CÓDIGOS AQUI EXEMPLIFICADOS TEM ESSENCIALMENTE O MESMO RESULTADO
